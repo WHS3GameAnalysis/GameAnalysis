08c0c22e7100d58f22828d3c2db46a44c0711f5f
Jihoon <godpd440@naver.com>
2025-06-18 00:31:36 +0900

    add OnGUI Test

---
+++ hack/GHBCheat/GHBCheat.cs
@@ -0,0 +1,27 @@
+﻿//using GameNetcodeStuff;
+//using HarmonyLib;
+//using LethalMenu.Language;
+//using LethalMenu.Manager;
+//using LethalMenu.Menu.Core;
+//using LethalMenu.Menu.Popup;
+//using LethalMenu.Themes;
+//using LethalMenu.Util;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Rendering;
+//using UnityEngine.Rendering.HighDefinition;
+
+namespace GHBCheat
+{
+    public class GHBCheat : MonoBehaviour
+    {
+        void OnGUI()
+        {
+            GUI.Label(new Rect(10, 10, 200, 40), "Hello Unity World!");
+        }
+    }
+}
---
+++ hack/GHBCheat/Loader.cs
@@ -0,0 +1,46 @@
+﻿//using LethalMenu.Handler;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+
+namespace GHBCheat
+{
+    public class Loader : MonoBehaviour
+    {
+        private static GameObject Load;
+
+        public static void Init()
+        {
+            if (Load != null) return;
+            //LoadAssembly("GHBCheat.Resources.0Harmony.dll");
+            //ChamHandler.ChamsSetEnabled(true);
+            Load = new GameObject();
+            Load.AddComponent<GHBCheat>();
+            Object.DontDestroyOnLoad(Load);
+        }
+
+        public static void LoadAssembly(string name)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Stream stream = assembly.GetManifestResourceStream(name);
+            byte[] rawAssembly = new byte[stream.Length];
+            stream.Read(rawAssembly, 0, (int)stream.Length);
+            AppDomain.CurrentDomain.Load(rawAssembly);
+        }
+
+        //public static void Unload()
+        //{
+        //    HackExtensions.ToggleFlags.Keys.ToList().ForEach(h => HackExtensions.ToggleFlags[h] = false);
+        //    ChamHandler.ChamsSetEnabled(false);
+        //    if ((bool)!LethalMenu.localPlayer?.playerActions.Movement.enabled) LethalMenu.localPlayer?.playerActions.Enable();
+        //    if (Cursor.visible && !LethalMenu.quickMenuManager.isMenuOpen) Cursor.visible = false;
+        //    LethalMenu.harmony.UnpatchAll("LethalMenu");
+        //    Object.Destroy(Load);
+        //}
+    }
+}
