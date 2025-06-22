086cfb45cf145b53bbb77de359d79d6eefd12b1a
iexplorer0011 <iexplorer0011@gmail.com>
2025-06-22 20:13:03 +0900

    GodMode 수정
    Harmony Prefix를 사용하여 데미지를 입지 않도록 수정하였습니다.

--- hack/LethalHack/LethalHack/Cheats/GodMode.cs
+++ hack/LethalHack/LethalHack/Cheats/GodMode.cs
@@ -1,12 +1,14 @@
 ﻿using GameNetcodeStuff;
 using UnityEngine;
+using HarmonyLib;
 
 
 namespace LethalHack.Cheats
 {
+    [HarmonyPatch]
     public class GodMode : Cheat // Cheat 클래스를 상속
     {
-       // public static PlayerControllerB localPlayer; // 스탯을 조작하기 위해 사용할 플레이어 객체
+        public static GodMode Instance = new GodMode(); // 싱글턴
 
         public override void Trigger() // 실제 기능을 구현한 메서드
         {
@@ -17,6 +19,13 @@ namespace LethalHack.Cheats
             }
 
             Hack.localPlayer.health = 100; // 체력 100으로 설정
-        }   
+        }
+        
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(PlayerControllerB), nameof(PlayerControllerB.DamagePlayer))]
+        public static bool NoDamage()
+        {
+            return !Instance.isEnabled;
+        }
     }
 }
