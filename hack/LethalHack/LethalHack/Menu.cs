ad8b57d4e00c3265fc7d78015781297f2ce9c1f3
ChoDeokCheol <gichan04@gmail.com>
2025-06-24 19:10:36 +0900

    GUI edit

--- hack/LethalHack/LethalHack/Menu.cs
+++ hack/LethalHack/LethalHack/Menu.cs
@@ -5,7 +5,7 @@ namespace LethalHack
     // GUI를 띄우는 역할
     public class Menu : MonoBehaviour
     {
-        private Rect windowRect = new Rect(20, 20, 200, 150); // GUI 창 위치, 크기 지정
+        private Rect windowRect = new Rect(20, 20, 200, 200); // GUI 창 위치, 크기 지정
         public bool showMenu = true; // 창 표시 여부
         
         public void Render() // 이 메서드는 Loader에서 호출되어서 GUI를 그리는 역할을 합니다.
@@ -29,8 +29,8 @@ namespace LethalHack
             Hack.Instance.Seed.isEnabled = GUI.Toggle(new Rect(10, 120, 180, 20), Hack.Instance.Seed.isEnabled, "Input Seed");
             if (Hack.Instance.Seed.isEnabled) //시드 선택 GUI
             {
-                GUI.Label(new Rect(30, 120, 180, 20), "Select Seed Type : ");
-                if(GUI.Button(new Rect(50, 120, 180, 20), LethalHack.Cheats.InputSeed.seedListName[Hack.Instance.Seed.selectedSeedIndex]))
+                GUI.Label(new Rect(30, 145, 180, 20), "Select Seed Type : ");
+                if(GUI.Button(new Rect(50, 165, 180, 20), LethalHack.Cheats.InputSeed.seedListName[Hack.Instance.Seed.selectedSeedIndex]))
                 {
                     Hack.Instance.Seed.selectedSeedIndex = (Hack.Instance.Seed.selectedSeedIndex + 1) % LethalHack.Cheats.InputSeed.seedListNumber.Length;
                 }
